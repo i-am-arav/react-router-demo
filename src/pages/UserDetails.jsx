83d3be5d0a3b3d15576768ae93d256656936212d
Aravind <aravindkumartechtrek@gmail.com>
2023-08-24 12:30:21 +0530

    5.1 Manipulate search Params

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -4,8 +4,11 @@ import { useParams, useSearchParams } from 'react-router-dom'
 const UserDetails = () => {
 const {userId} = useParams();
 const [searchParams, setSearchParams] = useSearchParams({name: ''});
+const userName = searchParams.get('name');
   return (
-    <div style={{margin:'20px'}}>I am user {userId} {searchParams.get('name') && <span>My Username is {searchParams.get('name')}</span>}</div>
+    <div style={{margin:'20px'}}>I am user {userId} {searchParams.get('name') && <span>My Username is {searchParams.get('name')}</span>}
+    <input value={userName} onChange={(e) => setSearchParams({name: e.target.value})}  />
+    </div>
   )
 }
 
