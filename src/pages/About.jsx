2add42eb56c08e29021ab5c6be71d1fa46931a1a
Aravind <aravindkumartechtrek@gmail.com>
2023-08-26 22:27:11 +0530

    7.1 consoling location

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 
 const About = () => {
-  const {state} = useLocation();
+  const location = useLocation();
+  console.log('location',location)
   return (
-    <div style={{margin:'20px'}}>About Page {state}</div>
+    <div style={{margin:'20px'}}>About Page {location.state}</div>
   )
 }
 
